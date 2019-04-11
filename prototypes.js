8f4ff526d2c44d14f61f49d504e23aa7670a16dc
Kamil Sobieraj <kamil.sobieraj.dev@gmail.com>
2019-04-11 10:13:19 +0200

    Simple prototype

--- prototypes.js
+++ prototypes.js
@@ -1,1 +1,16 @@
-console.log("works!");
+/*******************************Creating a prototype */
+function Circle(radius) {
+  this.radius = radius;
+  // this.draw = function() {
+  //   console.log(`Draw function constructor! ${this.radius}`);
+  // };
+}
+//We're moving draw method to prototype to save some space in memory - every Circle object willinherit it
+Circle.prototype.draw = function() {
+  console.log(`Draw function constructor! ${this.radius}`);
+};
+
+const c1 = new Circle(10);
+const c2 = new Circle(5);
+c1.draw();
+c2.draw();
